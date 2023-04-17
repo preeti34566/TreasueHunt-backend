3b4c4d68f78c1566d878d7c2f4805a1d01874acc
Preeti Kumari <sharmaaglina@gmail.com>
2023-04-17 09:06:17 +0530

    fixing: CORS and server access

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -6,7 +6,7 @@ import (
 
 func (app *application) enableCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://treasureh0nt.vercel.app/")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 
 		if r.Method == "OPTIONS" {
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
